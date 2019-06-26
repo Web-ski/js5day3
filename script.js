58296c3992b679c8fde874b0c3e09eebc78b93da
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2019-06-26 14:16:03 +0200

    Add variables

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 const carElement = document.createElement('div');
 const body = document.querySelector('body');
+const tickDuration = 100;
+const displacementPerTick = 10;
 let position = 0;
 
-carElement.className = 'car-game__car-element';
+function applyStylesToCar() {
+    carElement.className = 'car-game__car-element';
 
-carElement.style.width = '100px';
-carElement.style.height = '100px';
-carElement.style.backgroundColor = 'red';
-carElement.style.position = 'absolute';
-carElement.style.left = '0px';
+    carElement.style.width = '100px';
+    carElement.style.height = '100px';
+    carElement.style.backgroundColor = 'red';
+    carElement.style.position = 'absolute';
+    carElement.style.left = position + 'px';
 
-body.appendChild(carElement);
+    body.appendChild(carElement);
+}
 
-setInterval(
-    function(){
-        position = position + 10;
-        carElement.style.left = position + 'px';
-    },
-    1000
+function moveCar() {
+    position = position + displacementPerTick;
+    carElement.style.left = position + 'px';
+}
+
+applyStylesToCar();
+
+const intervalId = setInterval(
+    moveCar,
+    tickDuration
 )
