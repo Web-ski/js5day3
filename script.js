8b1623fda0447e76447bf3d709b5614175105042
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2019-06-27 09:10:40 +0200

    Add 0 velocity

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const carElement = document.createElement('div');
 const body = document.querySelector('body');
 const tickDuration = 10;
-const velocity = 100;
+const maxAcceleration = 10;
 
 let position = 0;
-let accelerate = false;
+let acceleration = 0;
+let velocity = 0;
 
 function applyStylesToCar() {
     carElement.className = 'car-game__car-element';
@@ -19,9 +20,11 @@ function applyStylesToCar() {
 }
 
 function moveCar() {
-    if (!accelerate) return;
-    const displacementPerTick = velocity * (tickDuration / 1000);
-    position = position + displacementPerTick;
+    const time = tickDuration / 1000;
+    const newPosition = position + velocity * time + (acceleration * time * time) / 2
+    position = newPosition < position ? position : newPosition;
+    const newVelocity = velocity = velocity + acceleration * time;
+    velocity = Math.max(0, newVelocity);
     carElement.style.left = position + 'px';
 }
 
@@ -36,7 +39,10 @@ window.addEventListener(
     'keydown',
     function (event) {
         if (event.key === 'a') {
-            accelerate = true;
+            acceleration = maxAcceleration;
+        }
+        if (event.key === 'd') {
+            acceleration = -maxAcceleration;
         }
     }
 )
@@ -45,7 +51,10 @@ window.addEventListener(
     'keyup',
     function (event) {
         if (event.key === 'a') {
-            accelerate = false;
+            acceleration = 0;
+        }
+        if (event.key === 'd') {
+            acceleration = 0;
         }
     }
 )
